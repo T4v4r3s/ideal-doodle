b9e0dcf0b0d512de64d74ba4f9db3a6a9b0f5a7b
Larihviolet <larissarochagoncalves2444@gmail.com>
2024-12-07 11:01:08 -0300

    Crie  funcao de imprimir na lista filmes utilizando uma cópia para não danifica-la

---
+++ recomendar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "filmes.h"
+#include "usuarios.h"
+
+int main(){
+    int numeroUSP;
+    int pessoa;
+    
+    // A primeira pergunta seria o quem e o usuario
+    printf("Qual o seu número USP, por favor ?")
+    scanf("%d", &numeroUSP);
+    pessoa = buscaUsuario(usuarioBloco *raiz, int numeroUSP);
+
+    // A segunda eh conseguir a lista de filmes do usuario 
+    pessoa.posicaoLista
+}
